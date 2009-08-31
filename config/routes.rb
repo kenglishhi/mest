6fe5908b9b898a0cb102e55ea3031eb67bfe7257
Kevin English <kenglish@gmail.com>
2009-08-31 10:31:47 -1000

    Added namespace route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :user_session
   map.resources :users
   map.resource :home
+  map.namespace(:admin) do |admin|
+    admin.resources :users
+  end
+
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map the login/logout urls
