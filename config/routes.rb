79fee7bee3fd4ad6613b17670df3620f89c835ae
Kevin English <kenglish@ohia.(none)>
2009-09-03 13:58:32 -1000

    Added blast cleaner controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :blast_results
+
+  map.resources :blasts
+  map.resources :blast_cleaners
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes"
   map.resources :fasta_files, :active_scaffold => true, :new => { :extract_sequences =>  :any } 
   map.resources :biodatabases, :active_scaffold => true
-  map.resources :blast, :active_scaffold => true
+#  map.resources :blast, :active_scaffold => true
 #  map.resources :adminusers, :controller => "admin/users", :active_scaffold => true
   map.resource :user_session
   map.resources :users
@@ -26,7 +31,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-#    map.connect ':controller/:action/:id'
+    map.connect ':controller/:action/:id'
   #  map.connect ':controller/:action/:id.:format'
 
 end
---
+++ test/unit/helpers/blast_cleaners_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class BlastCleanersHelperTest < ActionView::TestCase
+end
