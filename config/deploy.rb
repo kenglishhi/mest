646c269d6f87cd593036102f655b865a9e800368
Kevin English <kenglish@gmail.com>
2009-09-11 17:07:46 -1000

    Added stop/start for delayed job daemon

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,32 +14,49 @@ role :app, "gp202.ics.hawaii.edu"
 role :db,  "gp202.ics.hawaii.edu", :primary => true
 
 namespace :deploy do
+
   desc "Restart Application"
   task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
-    run "ruby #{current_path}/script/delayed_job restart production"
   end
-
-#  desc " Updates the symlink for database.yml file to the just deployed release."
-#  task :symlink do
-#    run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
-#  end
 
 end
 
 namespace :symlink do
+
   desc 'symlink database.yml'
   task :database_yml do
     run <<-CMD
       ln -fs #{shared_path}/config/database.yml #{release_path}/config/database.yml
     CMD
   end
+
 end
+namespace :delayed_job do
+
+  desc 'restart delayed job'
+  task :start do
+    run <<-CMD
+     ruby #{current_path}/script/delayed_job start production
+    CMD
+  end
+  task :stop do
+    run <<-CMD
+     ruby #{current_path}/script/delayed_job stop production 
+    CMD
+  end
+
+
+end
+
 
 #task :after_update_code, :roles => :app do
 #  puts "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml " 
 #  puts "------------------"
 #  run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
 #end
+
 after 'deploy:update_code', 'symlink:database_yml'
 after 'symlink:database_yml', 'deploy:migrate'
+before 'deploy:symlink', 'delayed_job:stop'
+after 'deploy:symlink', 'delayed_job:start'
