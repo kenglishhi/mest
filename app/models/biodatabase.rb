1b3fe7919393c842edc08351ce5f8ff07879ed25
Kevin English <kenglish@ohia.(none)>
2009-09-18 12:25:06 -1000

    Removed if statements from generate fast file routine

--- app/models/biodatabase.rb
+++ app/models/biodatabase.rb
@@ -8,7 +8,7 @@ class Biodatabase < ActiveRecord::Base
   has_many :biodatabase_biosequences, :dependent => :destroy
   has_many :biosequences, :through => :biodatabase_biosequences
   has_many :biodatabase_links, :dependent => :destroy
-  has_many :linked_biodatabase_links, :foreign_key =>'linked_biodatabase_id', :dependent => :destroy
+  has_many :linked_biodatabase_links, :class_name =>'BiodatabaseLink', :foreign_key =>'linked_biodatabase_id', :dependent => :destroy
 
   validates_presence_of :name
   validates_presence_of :biodatabase_type_id
@@ -16,29 +16,21 @@ class Biodatabase < ActiveRecord::Base
 
   def generate_fasta
     if fasta_file_id.nil?
-      if biodatabase_type.name == "GENERATED-MASTER"
-        children.each do | child |
-          child.generate_fasta
-        end
-      elsif   ["UPLOADED-CLEANED","GENERATED-MATCH" ].include? biodatabase_type.name
-        filename =  "#{name}.fasta"
-        fasta_file_handle = File.new(filename,"w")
-        biosequences.each do | seq |
-          fasta_file_handle.puts(seq.to_fasta)
-        end
-        fasta_file_handle.close
-        puts "generating fasta"
-
-        fasta_file_handle = File.new(filename,"r")
-        self.fasta_file = FastaFile.new
-        self.fasta_file.fasta = fasta_file_handle
-        self.fasta_file.is_generated = true
-        self.fasta_file.save
-        puts "new id #{self.fasta_file.errors.full_messages.to_sentence} "
-        save
-      else
-        logger.error("not generating fasta")
+      filename =  "#{name}.fasta"
+      fasta_file_handle = File.new(filename,"w")
+      biosequences.each do | seq |
+        fasta_file_handle.puts(seq.to_fasta)
       end
+      fasta_file_handle.close
+      puts "generating fasta"
+
+      fasta_file_handle = File.new(filename,"r")
+      self.fasta_file = FastaFile.new
+      self.fasta_file.fasta = fasta_file_handle
+      self.fasta_file.is_generated = true
+      self.fasta_file.save
+      puts "new id #{self.fasta_file.errors.full_messages.to_sentence} "
+      save
     end
   end
 
