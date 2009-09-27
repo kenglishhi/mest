06ce31eae4c29f7c891c02665380ba752c2453e8
Kenglish <kenglish@kalena.(none)>
2009-09-26 18:37:27 -1000

    test biodatabase id param check

--- app/controllers/tools/blast_create_dbs_controller.rb
+++ app/controllers/tools/blast_create_dbs_controller.rb
@@ -3,8 +3,8 @@ class Tools::BlastCreateDbsController < ApplicationController
     if params[:test_biodatabase_id]
 
     end
-    if Biodatabase.exists?(params[:biodatabase_id] )
-      @biodatabase = Biodatabase.find(params[:biodatabase_id] )
+    if Biodatabase.exists?(params[:test_biodatabase_id] )
+      @test_biodatabase = Biodatabase.find(params[:test_biodatabase_id] )
     end
     #    @blast_command = BlastCommand.new(:evalue => 0.0001, :query_fasta_file_id => @biodatabase.fasta_file_id)
     #    @blast_command.biodatabase_type_id = BiodatabaseType.find_by_name("UPLOADED-CLEANED").id
