1075664c6e5c2c04fafa32ba43d19a628980ed62
Kevin English <kenglish@ohia.(none)>
2009-10-08 17:23:59 -1000

    Cleanup of the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,26 @@
 ActionController::Routing::Routes.draw do |map|
-
-#  map.resources :workbench
-
-  map.resources :biodatabase_links, :active_scaffold => true
-  map.resources :biodatabase_link_types, :active_scaffold => true
-
+  # Active Scaffold Based controllers
   map.resources :biodatabase_groups, :active_scaffold => true
+  map.resources :biodatabase_link_types, :active_scaffold => true
+  map.resources :biodatabase_links, :active_scaffold => true
   map.resources :biodatabase_types, :active_scaffold => true
+  map.resources :biodatabases, :active_scaffold => true # , :member => { :generate_fasta => :post}
   map.resources :projects, :active_scaffold => true
   map.resources :blast_results, :active_scaffold => true
-  map.resources :blasts
   map.resources :fasta_files, :active_scaffold => true, :new => { :extract_sequences =>  :any } 
-  map.resources :biodatabases, :active_scaffold => true
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes"
-
-#  map.resources :blast, :active_scaffold => true
-#  map.resources :adminusers, :controller => "admin/users", :active_scaffold => true
   map.resource :user_session
   map.resources :users
-
   map.resource :home
+
   map.namespace(:admin) do |admin|
     admin.resources :users
   end
+
   map.namespace(:tools) do |tools|
     tools.resources :blast_cleaners
     tools.resources :blast_create_dbs
   end
+
   map.namespace(:jobs) do |jobs|
     jobs.resources :queued_jobs, :active_scaffold => true
     jobs.resources :running_jobs, :active_scaffold => true
@@ -56,9 +48,6 @@ ActionController::Routing::Routes.draw do |map|
 		:controller => 'workbench/home',
 		:action     => 'gemtest'
 
-
-
-
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map the login/logout urls
   map.login "login", :controller => "user_sessions", :action => "new"
@@ -69,7 +58,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-    map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id'
   #  map.connect ':controller/:action/:id.:format'
 
 end
