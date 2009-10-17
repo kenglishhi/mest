960c35d9f72fc61445b0bd2fcfd364e057bdc89e
Kevin English <kenglish@ohia.(none)>
2009-10-16 15:29:56 -1000

    Fixed slow biosequence controllet

--- app/controllers/biosequences_controller.rb
+++ app/controllers/biosequences_controller.rb
@@ -4,9 +4,9 @@ class BiosequencesController < ApplicationController
 
   active_scaffold :biosequences do |config|
     config.list.label = "Sequences"
-    config.columns = [:name, :seq, :length, :alphabet, :biodatabases]
-    config.columns[:biodatabases].includes = [:biodatabase_biosequences]
-    config.list.columns = [:name, :seq, :length, :alphabet,:biodatabases,:original_name]
+    config.columns = [:name, :seq, :length, :alphabet]
+    config.columns[:name].includes = [:biodatabase_biosequences]
+    config.list.columns = [:name, :seq, :length, :alphabet,:original_name]
     config.show.columns = [:name, :entire_seq, :length, :alphabet, :biodatabases,:original_name]
     config.actions.exclude :nested , :create, :update, :delete
   end
