de9e05938ea85c2403280bcdfed606dff44d6b6a
Kenglish <kenglish@kalena.(none)>
2009-10-17 22:02:41 -1000

    Facories

---
+++ test/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :job_log do |j|
+  j.job_name 'Extract Sequences EST_Clade_A.fastaSequences'
+  j.job_class_name  'Jobs::ExtractSequences'
+  j.started_at '2009-10-10 04:34:30'
+  j.stopped_at '2009-10-10 04:34:30'
+end
