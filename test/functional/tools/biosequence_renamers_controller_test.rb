483bd093b07a94202eac447e66892f2f24c57195
Kevin English <kenglish@ohia.(none)>
2009-10-23 15:20:55 -1000

    New tests for tool controllers

--- test/functional/tools/biosequence_renamers_controller_test.rb
+++ test/functional/tools/biosequence_renamers_controller_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../../test_helper'
 
 class Tools::BiosequenceRenamersControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  context "test create renamer job (html)" do
+  context "test create renamer job " do
     setup do
       u = users(:users_001)
       UserSession.create(u)
@@ -14,15 +14,14 @@ class Tools::BiosequenceRenamersControllerTest < ActionController::TestCase
       assert_response :redirect
       assert_equal @delayed_job_count+1,Job.count
     end
+
     should "Assert Should succeed on post to create with JSON" do
       assert true
       post :create, :format=> 'json',
         :biodatabase_id =>  biodatabases(:biodatabases_001).id, :prefix => 'xxx'
-#        :format => 'json'
       assert_response :success
       assert_match /Data Saved/, @response.body
+      assert_equal @delayed_job_count+1,Job.count
     end
-
-
   end
 end
--- test/functional/tools/blast_create_dbs_controller_test.rb
+++ test/functional/tools/blast_create_dbs_controller_test.rb
@@ -1,8 +1,32 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../../test_helper'
 
 class Tools::BlastCreateDbsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "test blast create dbs " do
+    setup do
+      u = users(:users_001)
+      UserSession.create(u)
+      @delayed_job_count =  Job.count
+    end
+    should "Assert Should succeed on post to create with HTML" do
+      assert true
+      post :create, :test_biodatabase_id =>  biodatabases(:biodatabases_001).id,
+        :target_biodatabase_id =>  biodatabases(:biodatabases_001).id,
+        :prefix => 'xxx'
+      assert_response :redirect
+      assert_equal @delayed_job_count+1,Job.count
+    end
+
+    should "Assert Should succeed on post to create with JSON" do
+      assert true
+      post :create, :format=> 'json', :test_biodatabase_id =>  biodatabases(:biodatabases_001).id,
+        :target_biodatabase_id =>  biodatabases(:biodatabases_001).id,
+        :prefix => 'xxx'
+      #:biodatabase_id =>  biodatabases(:biodatabases_001).id, :prefix => 'xxx'
+      assert_response :success
+      assert_match /Data Saved/, @response.body
+      assert_equal @delayed_job_count+1,Job.count
+    end
   end
+
 end
