89acf121adc3a196060bbce9608f4cc040bd649c
Kevin English <kenglish@ohia.(none)>
2009-11-06 12:58:56 -1000

    Changed sorting for results controller

--- app/controllers/blast_results_controller.rb
+++ app/controllers/blast_results_controller.rb
@@ -3,6 +3,7 @@ class BlastResultsController < ApplicationController
   active_scaffold :blast_results do |config|
     config.list.label = "Blast Results"
     config.list.columns = [:name, :output_file_name,:command, :output_file_size,:started_at, :stopped_at]
+    config.list.sorting = [{:started_at => :desc}]
 #    config.action_links.add "Blast", :parameters => {:controller => 'blast'},
 #    :action => 'index', :type => :table, :page => true
   end
