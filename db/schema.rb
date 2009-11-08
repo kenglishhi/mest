ea6369c3e439cd56b29165a99f64c1a469aa9f37
Kenglish <kenglish@kalena.(none)>
2009-11-07 21:51:13 -1000

    New schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091102054053) do
+ActiveRecord::Schema.define(:version => 20091108074423) do
 
   create_table "alignments", :force => true do |t|
     t.string   "label"
