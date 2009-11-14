a9e2365b28df7c07c406e3c10b413850acfceb4f
Kenglish <kenglish@kalena.(none)>
2009-11-13 17:09:11 -1000

    Fix to add FORM_AUTH_TOKEN

--- public/javascripts/workbench/base.js
+++ public/javascripts/workbench/base.js
@@ -1,2 +1,20 @@
 Ext.namespace('Ext.bio');
 Ext.namespace('Ext.bio.workbench');
+
+
+
+(function() {
+  var originalGetCallData = Ext.direct.RemotingProvider.prototype.getCallData;
+
+  Ext.override(Ext.direct.RemotingProvider, {
+    getCallData: function(t) {
+      var defaults = originalGetCallData.apply(this, arguments);
+
+      return Ext.apply(defaults, {
+        authenticity_token: FORM_AUTH_TOKEN
+      });
+    }
+  })
+})();
+
+
