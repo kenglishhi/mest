24d448c1280be230f69198ac20b298f00716c868
Kevin English <kenglish@kalalau.(none)>
2009-11-27 15:56:25 -1000

    New alignment grid, yay

---
+++ public/javascripts/workbench/alignment-grid.js
@@ -0,0 +1,28 @@
+/*!
+ * blast-result-grids.js
+ * Kevin English, University of Hawaii
+ */
+Ext.bio.AlignmentsGrid =  Ext.extend(Ext.bio.RestfulGrid, {
+  useEditorFlag:false,
+  usePagingBarFlag: true,
+  displayColumns:  [ {
+    header: "Label",
+    autoWidth: true,
+    sortable: true,
+    dataIndex: 'label'
+  }, {
+    header: "Alignment File Name",
+    autoWidth: true,
+    sortable: true,
+    dataIndex: 'alignment_file_name_display',
+    renderer: function(value, p, record, row, col, store) {
+      return '<a href="' + record.data.alignment_file_url +'" target="_blank"  >' + record.data.alignment_file_name_display + '</a>';
+    }
+  }
+  ],
+  initComponent: function() {
+    Ext.bio.AlignmentsGrid.superclass.initComponent.call(this);
+  }
+});
+
+Ext.reg('alignments-grid', Ext.bio.AlignmentsGrid);
