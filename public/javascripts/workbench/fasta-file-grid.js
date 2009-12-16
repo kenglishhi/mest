4d69101dbcf96f2b90e0724133c8bc615edb59b4
Kevin English <kenglish@kalalau.(none)>
2009-12-15 18:06:02 -1000

    New grid fields for fasta file grid

--- public/javascripts/workbench/fasta-file-grid.js
+++ public/javascripts/workbench/fasta-file-grid.js
@@ -11,11 +11,6 @@ Ext.bio.FastaFileGrid =  Ext.extend(Ext.bio.RestfulGrid, {
     sortable: true,
     dataIndex: 'label'
   }, {
-    header: "File Size",
-    autoWidth: true,
-    sortable: true,
-    dataIndex: 'fasta_file_size'
-  }, {
     header: "Fasta File Name",
     autoWidth: true,
     sortable: true,
@@ -23,7 +18,12 @@ Ext.bio.FastaFileGrid =  Ext.extend(Ext.bio.RestfulGrid, {
     renderer: function(value, p, record, row, col, store) {
       return '<a href="' + record.data.fasta_file_url + '" target="_blank"  >' + record.data.fasta_file_name_display + '</a>';
     }
-  }],
+  }, {
+    header: "File Size",
+    autoWidth: true,
+    sortable: true,
+    dataIndex: 'fasta_file_size_display'
+  } ],
   initComponent: function() {
     Ext.bio.FastaFileGrid.superclass.initComponent.call(this);
   } ,
