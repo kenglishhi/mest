45e85c0998ccc87040c4290ddfbc3bde0ea9a303
Kevin English <kenglish_hi@yahoo.com>
2010-01-14 11:49:42 -1000

    Fixed bugs from broken test, related to return true in loggable

--- lib/jobs/blast_and_create_dbs.rb
+++ lib/jobs/blast_and_create_dbs.rb
@@ -6,7 +6,7 @@ class Jobs::BlastAndCreateDbs< Jobs::AbstractJob
 
   def do_perform
     [ :test_biodatabase_id, :target_biodatabase_ids].each do | required_param_key |
-       raise "Error #{required_param_key} can not be blank!" if params[required_param_key].blank?
+      raise "Error #{required_param_key} can not be blank!" if params[required_param_key].blank?
     end
     blast_command = Blast::CreateDbs.new(params)
     blast_command.run
