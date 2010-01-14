d8356ee390fdb2b626a0f8ae17de3bfb22baabbf
Kevin English <kenglish_hi@yahoo.com>
2010-01-14 10:57:02 -1000

    Allow selection on the grid

--- public/javascripts/workbench/base.js
+++ public/javascripts/workbench/base.js
@@ -42,3 +42,11 @@ Ext.workbenchMenu.blastHelpWindow = new Ext.Window({
     }
   }]
 });
+if (!Ext.grid.GridView.prototype.templates) {
+   Ext.grid.GridView.prototype.templates = {};
+}
+Ext.grid.GridView.prototype.templates.cell = new Ext.Template(
+   '<td class="x-grid3-col x-grid3-cell x-grid3-td-{id} x-selectable {css}" style="{style}" tabIndex="0" {cellAttr}>',
+   '<div class="x-grid3-cell-inner x-grid3-col-{id}" {attr}>{value}</div>',
+   '</td>'
+);
